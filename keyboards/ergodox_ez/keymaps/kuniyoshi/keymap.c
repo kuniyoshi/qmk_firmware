59079c4ed7b82168df2eedb83a7130617b1393a6
KUNIYOSHI Kouji <kuniyoshi.kouji@gmail.com>
2019-05-06 16:09:21 +0900

    Make type `_` easier

--- keyboards/ergodox_ez/keymaps/kuniyoshi/keymap.c
+++ keyboards/ergodox_ez/keymaps/kuniyoshi/keymap.c
@@ -17,13 +17,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  CAPS  |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |  TAB   |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |   -    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |   =    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |   `  |   -  | Left | Right|                                       |  Up  | Down |   [  |   ]  |      |
+ *   | TAB  |   `  |   -  | Left | Right|                                       |  Up  | Down |   [  |   ]  |   _  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Esc  |      |       |      | Ent  |
@@ -36,10 +36,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(
   // left hand
   KC_CAPS,         KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_NO,
-  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_NO,
+  KC_MINS,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    KC_NO,
   KC_EQL,          KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
   KC_LSFT,         KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_NO,
-  KC_NO,           KC_GRAVE,    KC_MINS,       KC_LEFT, KC_RGHT,
+  KC_TAB,          KC_GRAVE,    KC_MINS,       KC_LEFT, KC_RGHT,
                                                                   KC_ESC, KC_NO,
                                                                           LCMD_T(KC_HANJ),
                                                         KC_BSPC, KC_LCTL, KC_LALT,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,        KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
                 KC_H,    KC_J,    KC_K,    KC_L,              KC_SCOLON,      KC_QUOT,
   KC_NO,        KC_N,    KC_M,    KC_COMM, KC_DOT,            KC_SLSH,        KC_RSFT,
-  KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, KC_NO,
+  KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, S(KC_MINS),
   KC_NO,   KC_ENT,
   RCMD_T(KC_HAEN),
   KC_RALT, KC_LCTL, KC_SPC
